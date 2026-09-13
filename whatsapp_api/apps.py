c50e1c25f295ef914f65041e1b3ff52eb366f827
pk8873 <Chaudharystar051@gmail.com>
2026-09-13 07:52:14 +0530

    Add WhatsApp API app config

---
+++ whatsapp_api/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WhatsappApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "whatsapp_api"
+    verbose_name = "WhatsApp API"
